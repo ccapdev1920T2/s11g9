d0be41f217877f215d0adbd0acebb153cbba250a
GaboMarcelo <58316241+GaboMarcelo@users.noreply.github.com>
2020-04-30 16:54:33 +0800

    validation

--- public/js/profile.js
+++ public/js/profile.js
@@ -3,33 +3,49 @@ $(document).ready(function () {
         var fName = validator.trim($('#fName').val());
         var lName = validator.trim($('#lName').val());
         var email = validator.trim($('#email').val());
-        // var pw = validator.trim($('#pw').val());
+        var pw = validator.trim($('#pw').val());
+        var cpw = validator.trim($('#cpw').val());
 
         var fNameEmpty = validator.isEmpty(fName);
         var lNameEmpty = validator.isEmpty(lName);
         var emailEmpty = validator.isEmpty(email);
-        // var pwEmpty = validator.isEmpty(pw);
+        var pwEmpty = validator.isEmpty(pw);
+        var cpwEmpty = validator.isEmpty(cpw);
 
-        return !fNameEmpty && !lNameEmpty && !emailEmpty;
+        return !fNameEmpty && !lNameEmpty && !emailEmpty && !pwEmpty && !cpwEmpty;
     }
-    // function isValidPassword(field) {
-    //     var validPassword = false;
 
-    //     var password = validator.trim($('#pw').val());
-    //     var isValidLength = validator.isLength(password, {min: 4, max: 32});
+    function isValidPassword(field) {
+        var validPassword = false;
 
-    //     if(isValidLength) {
-    //         if(field.is($('#pw')))
-    //             $('#pwError').text('');
+        var password = validator.trim($('#pw').val());
+        var isValidLength = validator.isLength(password, {min: 4, max: 32});
 
-    //         validPassword = true;    
-    //     } else {
-    //         if(field.is($('#pw')))
-    //             $('#pwError').text('Password should contain at least 4 characters and 32 characters at most.');
-    //     }
+        if(isValidLength) {
+            if(field.is($('#pw')))
+                $('#pwError').text('');
 
-    //     return validPassword;
-    // }
+            validPassword = true;    
+        } else {
+            if(field.is($('#pw')))
+                $('#pwError').text('Password should contain at least 4 characters and 32 characters at most.');
+        }
+
+        return validPassword;
+    }
+
+    function isValidCPass(field) {
+        var validCPass = false;
+        
+        if(field == document.getElementById('pw').value) {
+            validCpass = true;
+        } else {
+            if(field.is($('#cpw')))
+                $('#cpwError').text('Passwords do not match.');
+        }
+
+        return validCPass;
+    }
 
     function validateField(field, fieldName, error) {
         var value = validator.trim(field.val());
@@ -43,10 +59,10 @@ $(document).ready(function () {
         }
 
         var filled = isFilled();
-        // var validPassword = isValidPassword(field);
+        var validPassword = isValidPassword(field);
+        var validConfirmPassword = isValidCPass(field);
 
-        // if(filled && validPassword) {
-        if (filled){
+        if (filled && validPassword && validConfirmPassword) {
             $('#submit').prop('disabled', false);
         }
         else{
@@ -66,7 +82,11 @@ $(document).ready(function () {
         validateField($('#email'), 'Email', $('#emailError'));
     });
 
-    // $('#pw').keyup(function () {
-    //     validateField($('#pw'), 'Password', $('#pwError'));
-    // });
+    $('#pw').keyup(function () {
+        validateField($('#pw'), 'Password', $('#pwError'));
+    });
+
+    $('#cpw').keyup(function () {
+        validateField($('#cpw'), 'Confirm Password', $('#cpwError'));
+    });
 });
