b9b3b1e5af8ff1380625f8964824259a2c6f8c6b
carlobearr <carlo_luis_v_santos@dlsu.edu.ph>
2020-05-01 23:51:57 +0800

    db heroku fix

--- model/db.js
+++ model/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
-const url = 'mongodb://localhost:27017/nodekb';
-// const url = process.env.MONGODB_URL;
+// const url = 'mongodb://localhost:27017/nodekb';
+const url = process.env.MONGODB_URL;
 
 const options = {
     useUnifiedTopology: true,
