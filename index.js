92665120b5d0a152effed4b36aec410d48a13c44
Kate Magbitang <julianne_magbitang@dlsu.edu.ph>
2020-05-01 17:03:08 +0800

    changed port

--- index.js
+++ index.js
@@ -14,7 +14,8 @@ const routes = require('./routes/routes.js');
 const db = require('./model/db.js');
 
 const app = express()
-const port = 3000
+// const port = 3000
+const port = process.env.PORT || 3000;
 
 // passport configuration
 require('./helpers/passport')(passport);
